f82ce20cf1b6f7c67e7cea40a1d7390e64722851
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-01-17 23:00:48 +0900

    開発環境作成

---
+++ src/client/environments/environment.local.ts
@@ -0,0 +1,16 @@
+/**
+ * 環境変数local
+ */
+export const environment = {
+    production: false,
+
+    ENV: 'local',
+
+    API_ENDPOINT: 'https://oyatsu-cinerino-api-dev.appspot.com',
+
+    ENTRANCE_SERVER_URL: '',
+    WAITER_SERVER_URL: '',
+
+    ANALYTICS_ID: ''
+
+};
---
+++ src/client/environments/environment.ts
@@ -0,0 +1,16 @@
+/**
+ * 環境変数dev
+ */
+export const environment = {
+    production: false,
+
+    ENV: 'development',
+
+    API_ENDPOINT: 'https://oyatsu-cinerino-api-dev.appspot.com',
+
+    ENTRANCE_SERVER_URL: '',
+    WAITER_SERVER_URL: '',
+
+    ANALYTICS_ID: ''
+
+};
